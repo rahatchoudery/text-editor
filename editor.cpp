b3a0e932ddbed51eba7fc492b36e363fc9ce04ed
Rahat Choudery <55643679+rahatchoudery@users.noreply.github.com>
2020-01-30 22:00:58 -0500

    Create editor.cpp

---
+++ editor.cpp
@@ -0,0 +1,66 @@
+//rahat choudery
+#include <iostream>
+#include <fstream>
+#include <stack>
+using namespace std;
+
+int main ()
+{
+  ifstream fin;
+  char d;
+  stack <char> words;
+  stack <char> ordered_words;
+
+  fin.open("proj3test.txt"); //open the file
+
+  if (fin.fail())
+    {
+      cout << "Error!" << endl;
+    } //return error if file does not open
+
+  else
+    {
+      while (!fin.eof()) //while it is not the end of the file
+	{
+	  d = fin.get();
+
+	  if (d == '#')
+	    {
+	      words.pop();
+	    } //erase character
+
+	  else if (d == '$')
+	    {
+	      words.push(d);
+	    } //substitute character
+
+	  else if (d == '@')
+	    {
+	      while (!words.empty())
+		{
+		  words.pop();
+		}
+	    } //kill character
+
+	  else
+	    {
+	      words.push(d);
+	    } //if character is not special, push into stack
+	} //end while loop
+
+      while (!words.empty())
+	{
+	  ordered_words.push(words.top());
+	  words.pop();
+	} //populate second stack with correct ordering of words
+
+      while (!ordered_words.empty())
+	{
+	  cout << ordered_words.top();
+	  ordered_words.pop();
+	} //print ordered words
+
+      cout << endl;
+      fin.close();
+    } //end else
+} //end main
